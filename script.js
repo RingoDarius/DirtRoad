d2b03d74e7d2aa05f1cd292a557d109d86b401c4
= <kmurphy@weber.edu>
2026-05-04 10:08:47 -0600

    Updated Nav

--- script.js
+++ script.js
@@ -1,7 +1,65 @@
+/*
+_____________________________________________________________________________
+                       Navigation Links
 
+*/
+let links = [
+    {
+        "pageName": "About",
+        "URL": "index.html"
+    },
+    {
+        "pageName": "Events",
+        "URL": "events.html"
+    },
+    {
+        "pageName": "Music",
+        "URL": "music.html"
+    },
+    {
+        "pageName": "Contact",
+        "URL": "contact.html"
+    }
+    
+];
+
+
+
+
+
+var mylist = document.createElement('ul');
+
+mylist.setAttribute("class", "menu");
+mylist.setAttribute("id", "menu")
+
+let txt = "";
+for(var i of links){
+    var atag = document.createElement('a');
+    atag.setAttribute("href", i.URL);
+    atag.textContent = i.pageName;
+    var liElementp = document.createElement('li');
+    liElementp.appendChild(atag);
+    mylist.appendChild(liElementp);
+  
+}
+
+document.getElementById("nav").appendChild(mylist);
+  
+  
+  
+  
+  /*
+_____________________________________________________________________________
+                       Hamburger Menu
+
+*/
+  
+  
   const hamburger = document.getElementById('hamburger');
   const menu = document.getElementById('menu');
 
   hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
   });
+
+
